27e4a7e99854ce2e392e9e24725a3629575de0d3
dirtyredz <dirtyredz@live.com>
2017-01-14 11:50:19 -0600

    Added unit testing, and babel config

---
+++ UnitTesting/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ScrollUpButton from '../src/react-scroll-up-button';
+
+const rootEl = document.getElementById('ReactRoot');
+ReactDOM.render(
+  <ScrollUpButton />
+  ,rootEl
+);
