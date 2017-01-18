a39b44855e0c103324d9ef790a0436a7f66b1e65
dirtyredz <dirtyredz@live.com>
2017-01-18 09:44:26 -0600

    Final NodeJS fix

--- src/test/test.js
+++ src/test/test.js
@@ -1,8 +1,9 @@
+
 import { jsdom } from 'jsdom';
 
 var exposedProperties = ['window', 'navigator', 'document'];
 
-global.document = jsdom('<!doctype html><html><body></body></html>');
+global.document = jsdom('<!doctype html><html><body style="height:2000px"></body></html>');
 global.window = document.defaultView;
 Object.keys(document.defaultView).forEach((property) => {
   if (typeof global[property] === 'undefined') {
@@ -17,35 +18,51 @@ require('raf').polyfill()
 import React from 'react';
 import { mount } from 'enzyme';
 import sinon from 'sinon';
+import TestUtils from 'react-addons-test-utils';
 import ScrollUpButton from '../react-scroll-up-button';
 import { expect } from 'chai';
 
 
 //Testing group
-describe('Testing <ScrollUpButton/> Action scroll to props assigned top:', ()=>{
-  //it test a single requirment inside the test group
-  //did it scroll the page up
-  it('did scroll the page to 50', (done) => {
+describe('Testing <ScrollUpButton/> Action scroll to assigned props top:', ()=>{
+  let Component, ScrollTo_Stub;
+  before(()=>{
     window.pageYOffset = 0
-    let wrapper = mount(<ScrollUpButton StopPosition={50}/>);
-    //Settup stub and replace scrollTo function with ours.
+    //Render component
+    Component = mount(<ScrollUpButton StopPosition={50}/>);
 
-    let scrollTo_Stub = sinon.stub(window, 'scrollTo').callsFake((x,y)=>{
+    ////////////////////////////////
+    // Fix for NodeJS 7.4, without this line the test worked on NodeJS 6.9
+    // without window.scrollTo I would get the TypeError message below
+    // TypeError: Attempted to wrap scrollTo which is already stubbed
+    window.scrollTo = null;
+    // I only need to assign this value here once, even though I stub window.scrollTo at the bottom of the page
+    // Any advice would be much appeciated on explaining this behavior.
+    // The only thing I can suspect is nodeJS for some reason does something to the window obj or jsdom is failing to invoce the proper function in NodeJS 7.4
+    ////////////////////////////////
+
+    //Settup stub and replace scrollTo function with ours.
+    ScrollTo_Stub = sinon.stub(window, 'scrollTo').callsFake((x,y)=>{
       window.pageXOffset = x;
       window.pageYOffset = y;
-      wrapper.instance().HandleScroll(); // <-- call HandleScroll so the test can simulate the button being toggled
+      Component.instance().HandleScroll(); // <-- call HandleScroll so the test can simulate the button being toggled
     });
-    expect(wrapper.state().ToggleScrollUp).to.equal('');
+  })
+  after(()=>{
+    ScrollTo_Stub.restore(); // <-- Restore the objects method
+  })
+  //did it scroll the page up
+  it('did scroll the page to 50', (done) => {
+    expect(Component.state().ToggleScrollUp).to.equal(''); // <-- Is the button hidden
     window.pageYOffset = 300 // <-- scroll window down to prepare for smulation
-    wrapper.instance().HandleScroll(); // <-- call handleScroll since we scrolled the window down.
-    wrapper.instance().HandleClick(); // <-- call HandleClick to start the scroll up simulation.
-
+    Component.instance().HandleScroll(); // <-- call handleScroll since we scrolled the window down.
+    expect(Component.state().ToggleScrollUp).to.equal(true); // <-- Is the button visible
+    Component.instance().HandleClick(); // <-- call HandleClick to start the scroll up simulation.
+    //Well wait a little bit to let the simulation complete
     setTimeout(()=>{
-      expect(scrollTo_Stub.lastCall.args[1]).to.within(40,60);
-      expect(wrapper.state().ToggleScrollUp).to.equal('');
+      expect(ScrollTo_Stub.lastCall.args[1]).to.within(40,60); // <-- is pageYOffset between 40 and 60
+      expect(Component.state().ToggleScrollUp).to.equal(''); // <-- Button should be hidden again
       done() // <-- since were asynchronous with setTimeout instruct chai that were done with the test.
-      scrollTo_Stub.restore();
-      window.scrollTo.restore();
     }, 500);
   });
 });
@@ -127,28 +144,34 @@ describe('Testing <ScrollUpButton/> current state:', ()=>{
 
 //Testing group
 describe('Testing <ScrollUpButton/> Action scroll to default top:', ()=>{
-  //it test a single requirment inside the test group
-  //did it scroll the page up
-  it('did scroll the page to 0', (done) => {
+  let Component, ScrollTo_Stub;
+  before(()=>{
     window.pageYOffset = 0
-    let wrapper = mount(<ScrollUpButton />);
+    //Render component
+    Component = mount(<ScrollUpButton />);
     //Settup stub and replace scrollTo function with ours.
-    let scrollTo_Stub = sinon.stub(window, 'scrollTo').callsFake((x,y)=>{
+    // TypeError: Attempted to wrap scrollTo which is already stubbed
+    ScrollTo_Stub = sinon.stub(window, 'scrollTo').callsFake((x,y)=>{
       window.pageXOffset = x;
       window.pageYOffset = y;
-      wrapper.instance().HandleScroll(); // <-- call HandleScroll so the test can simulate the button being toggled
+      Component.instance().HandleScroll(); // <-- call HandleScroll so the test can simulate the button being toggled
     });
-    expect(wrapper.state().ToggleScrollUp).to.equal('');
+  })
+  after(()=>{
+    ScrollTo_Stub.restore(); // <-- Restore the objects method
+  })
+  //did it scroll the page up
+  it('did scroll the page to 0', (done) => {
+    expect(Component.state().ToggleScrollUp).to.equal(''); // <-- Is the button hidden
     window.pageYOffset = 300 // <-- scroll window down to prepare for smulation
-    wrapper.instance().HandleScroll(); // <-- call handleScroll since we scrolled the window down.
-    wrapper.instance().HandleClick(); // <-- call HandleClick to start the scroll up simulation.
-
+    Component.instance().HandleScroll(); // <-- call handleScroll since we scrolled the window down.
+    expect(Component.state().ToggleScrollUp).to.equal(true); // <-- Is the button visible
+    Component.instance().HandleClick(); // <-- call HandleClick to start the scroll up simulation.
+    //Well wait a little bit to let the simulation complete
     setTimeout(()=>{
-      expect(scrollTo_Stub.lastCall.args[1]).to.within(-10,10);
-      expect(wrapper.state().ToggleScrollUp).to.equal('');
+      expect(ScrollTo_Stub.lastCall.args[1]).to.within(-10,10); // <-- is pageYOffset between -10 and 10
+      expect(Component.state().ToggleScrollUp).to.equal(''); // <-- Button should be hidden again
       done() // <-- since were asynchronous with setTimeout instruct chai that were done with the test.
-      scrollTo_Stub.restore();
-      window.scrollTo.restore();
     }, 500);
   });
 });
