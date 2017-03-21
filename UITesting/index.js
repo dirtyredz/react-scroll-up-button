a208a473b84b459233e8be258d812210f72e0891
dirtyredz <dirtyredz@live.com>
2017-03-21 11:01:51 -0500

    Added Mount and Unmount function to UITesting

--- UITesting/index.js
+++ UITesting/index.js
@@ -2,11 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ScrollUpButton from '../src/react-scroll-up-button';
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    if(this.state.isToggleOn){
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'Mount react-scroll-up-button' : 'Unmount react-scroll-up-button'}
+        </button>
+      );
+    }else{
+      return (
+        <div>
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'Mount react-scroll-up-button' : 'Unmount react-scroll-up-button'}
+        </button>
+        <ScrollUpButton StopPosition={0}/>
+      </div>
+      );
+
+    }
+  }
+}
+
 const rootEl = document.getElementById('ReactRoot');
 ReactDOM.render(
   <div>
-    <h1>TEST</h1>
-    <ScrollUpButton StopPosition={0}/>
+    <h1>REACT-SCROLL-UP-BUTTON</h1>
+    <Toggle/>
+
   </div>
   ,rootEl
 );
